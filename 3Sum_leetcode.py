131549f989c8cae3f9c5abb82e7b79e703077a13
Chetan <43693378+chetangaur@users.noreply.github.com>
2021-11-23 21:39:28 +0530

    Update 3Sum_leetcode.py

--- 3Sum_leetcode.py
+++ 3Sum_leetcode.py
@@ -1,36 +1,36 @@
-nums=[-1,0,1,2,-1,-4]                        # method 1 
-res=[]
-store_dict={}
-l=len(nums)
-nums.sort()
-for ele in nums:
-    if ele not in store_dict:
-        store_dict[ele]=1
-    else:
-        store_dict[ele]=store_dict[ele]+1
-print(store_dict)
-for i in range(l):
-    t2=store_dict[nums[i]]                                 # need to modify some code here
-    store_dict.pop(nums[i])        
+# nums=[-1,0,1,2,-1,-4]                        # method 1 
+# res=[]
+# store_dict={}
+# l=len(nums)
+# nums.sort()
+# for ele in nums:
+#     if ele not in store_dict:
+#         store_dict[ele]=1
+#     else:
+#         store_dict[ele]=store_dict[ele]+1
+# print(store_dict)
+# for i in range(l):
+#     t2=store_dict[nums[i]]                                 # need to modify some code here
+#     store_dict.pop(nums[i])        
 
-    for j in range(i+1,l):
-        t1=store_dict[nums[j]]
-        store_dict.pop(nums[j])
-        k=nums[i]+nums[j]
-        if -k in store_dict:
-            res.append([nums[i],nums[j],-k])
-        store_dict[nums[j]]=t1
-    store_dict[nums[i]]=t2
+#     for j in range(i+1,l):
+#         t1=store_dict[nums[j]]
+#         store_dict.pop(nums[j])
+#         k=nums[i]+nums[j]
+#         if -k in store_dict:
+#             res.append([nums[i],nums[j],-k])
+#         store_dict[nums[j]]=t1
+#     store_dict[nums[i]]=t2
         
-print(res)
-for ele in res:
-    ele.sort()
-print(res)
-ans=[]
-for ele in res:
-    if ele not in ans:
-        ans.append(ele)
-print(ans)        
+# print(res)
+# for ele in res:
+#     ele.sort()
+# print(res)
+# ans=[]
+# for ele in res:
+#     if ele not in ans:
+#         ans.append(ele)
+# print(ans)        
   
   
   
